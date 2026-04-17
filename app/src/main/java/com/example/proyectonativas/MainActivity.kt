21ec9bc1f4596912b2deb50bcbf7403d89c5a5e5
Paooo666 <paolayazuwu03@gmail.com>
2026-04-17 05:20:44 -0600

    implementacion de menu de ajustes

--- app/src/main/java/com/example/proyectonativas/MainActivity.kt
+++ app/src/main/java/com/example/proyectonativas/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.proyectonativas
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,12 +12,31 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        btnStart.setOnClickListener {
+            // Aquí llamarás al Fragment o Activity del combate
+            Toast.makeText(this, "¡Iniciando combate!", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnAjustes = findViewById<Button>(R.id.btn_settings)
+        btnAjustes.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val inflater = layoutInflater
+            val dialogView = inflater.inflate(R.layout.dialog_ajustes, null)
+
+            builder.setView(dialogView)
+            val dialog = builder.create()
+
+            // Quitar el fondo blanco feo que trae por defecto el AlertDialog
+            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+            dialogView.findViewById<Button>(R.id.btn_guardar).setOnClickListener {
+                dialog.dismiss() // Cerrar al guardar
+            }
+
+            dialog.show()
         }
     }
 }
